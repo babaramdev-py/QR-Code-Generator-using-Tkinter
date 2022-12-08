1643d2660947df2158666f06bd5ff0229f48f1fc
Ved Korlahalli <70955647+babaramdev-py@users.noreply.github.com>
2022-12-08 21:47:30 +0530

    Update open.py

--- QR_GEN/QRApp/open.py
+++ QR_GEN/QRApp/open.py
@@ -4,3 +4,5 @@ cairosvg.svg2png(url='myqr.svg',write_to='image.png')
 
 im = Image.open(r"/home/ved/Desktop/QR_GEN/QRApp/image.png")
 im.show()
+# A test file to check whether the CairoSVG works properly. 
+# Test report is positive and can be used to convert SVG to PNG easily in a matter of 2 seconds!
